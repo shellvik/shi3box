70b11d0119c4a893d00e9c914347b58035e11cf5
shellvik <souvikbiswas6663@gmail.com>
2026-02-17 01:39:30 +0530

    i3bar needs fixing

--- init.sh
+++ init.sh
@@ -135,6 +135,11 @@ cd ../..
 
 #----------------------
 
+# Download Sublime text
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo tee /etc/apt/keyrings/sublimehq-pub.asc > /dev/null
+echo -e 'Types: deb\nURIs: https://download.sublimetext.com/\nSuites: apt/stable/\nSigned-By: /etc/apt/keyrings/sublimehq-pub.asc' | sudo tee /etc/apt/sources.list.d/sublime-text.sources
+sudo apt-get update
+sudo apt-get install sublime-text
 
 
 
